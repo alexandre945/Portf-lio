a340e669b399c34d2c881578fc6c62f1f9042810
alexandre <alexandresousaroberto@gmail.com>
2022-02-11 19:09:04 -0300

    corrigindo bug no modal

--- resources/views/Portfolios/user.blade.php
+++ resources/views/Portfolios/user.blade.php
@@ -36,38 +36,40 @@
                             <form action="{{ route('message.delete',$datas->id)}}" method="post">
                                 @method('DELETE')
                                 @csrf
-                                <a href="#"data-bs-toggle="modal" data-bs-target="#exampleModal"><button id="excluir" class="btn btn-danger" onclick="preventDefault()">Excluir</button></a>
+                                <button id="excluir" class="btn btn-danger" onclick="preventDefault()"> Excluir </button>
+                                   
+                               
                             </form>
                             <a href="#"><button id="update" class="btn btn-success"> Edit</button></a>
                         </div>
                     </td>
                 </tr>
                 @endforeach
-            </tbody>
+            </tb    ody>
         </table>
     </div>
-<form action="{{route('message.delete',$datas->id) }}" method= "post">
-    @csrf 
-    @method('DELETE')
-<div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-  <div class="modal-dialog">
-    <div class="modal-content">
-      <div class="modal-header">
-        <h5 class="modal-title" id="exampleModalLabel">EXCLUIR</h5>
-        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-      </div>
-      <div class="modal-body">
-        <p>Deseja excluir este intem?</p>
-      </div>
-      <div class="modal-footer">
-        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">cancelar</button>
-        <button type="submit" class="btn btn-danger">Excluir</button>
-      </div>
-    </div>
-  </div>
-</div>
+    <form action="{{route('message.delete',$datas->id) }}" method="post">
+        @csrf
+        @method('DELETE')
+        <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+            <div class="modal-dialog">
+                <div class="modal-content">
+                    <div class="modal-header,btn btn-danger">
+                        <h5 class="modal-title" id="exampleModalLabel">COMFIRMAÇÃO</h5>
+                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                    </div>
+                    <div class="modal-body">
+                        <p>Deseja excluir este intem?</p>
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-warning" data-bs-dismiss="modal">cancelar</button>
+                        <button type="submit" class="btn btn-danger">Excluir</button>
+                    </div>
+                </div>
+            </div>
+        </div>
 
-</form>
+    </form>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous">
